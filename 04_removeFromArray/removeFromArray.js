602cf13349ed27c621e03d52fc9ac479fdad3de3
Monito <phil.menard1668@gmail.com>
2022-07-26 20:57:57 -0300

    Finish exercice 4 and 5.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
 
+    let table = arguments[0];
+
+    // I start the loop at 1 because 0 is the array (first argument)
+    for (let i = 1; i < arguments.length; i++) {
+
+        for (let j = 0; j <= table.length; j++) {
+            if (table[j] === arguments[i]) {
+                table.splice(j, 1);
+            }
+        }
+    }
+    return table;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
 
+    if (numberOne < 0 || numberTwo < 0) {
+        return "ERROR";
+    }
+    else if (typeof numberOne != "number" || typeof numberTwo != "number")  {
+        console.log("prout test fonctionne")
+        return "ERROR";
+    }
+    console.log(typeof numberOne);
+    let result = 0;
+    let index = 0;
+    let limit = 0;
+
+    if (numberOne < numberTwo)  {
+        index = numberOne
+        limit = numberTwo
+    }
+    else    {
+        index = numberTwo
+        limit = numberOne
+    }
+
+    for (let i = index; i <= limit; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
