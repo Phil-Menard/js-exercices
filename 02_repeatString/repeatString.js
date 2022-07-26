0c50bb9428bbb017aed381aaa011142635ab1c17
Monito <phil.menard1668@gmail.com>
2022-07-25 23:23:35 -0300

    Finish exercices 1, 2 and 3.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+
+    let coefficient = number;
+    
+    if (coefficient < 0)    {result = "ERROR"}
+    else  {
+        result = "";
+        let strings = [""];
+        let index = 0;
+
+        for (index; index < coefficient; index++) {
+            strings.push(string);
+        }
+
+        result = strings.join("");
+        }
+
+    return result;
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
 
+    let letters = "";
+
+    for (let i = stringToReverse.length - 1; i >= 0; i--)    {
+        letters += stringToReverse[i];
+    }
+    return letters;
 };
 
 // Do not edit below this line
