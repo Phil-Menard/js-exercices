98550450b00389ef581a6c71b9198e943b72c815
Monito <phil.menard1668@gmail.com>
2022-07-27 12:08:29 -0300

    Finish exercice 9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
-};
+    let str = string.toLowerCase();
+    
+    let value = str.replace(/[^a-zA-Z]/g, '');
+    value.toLowerCase();
+    
+    let valueSize = value.length;
+    let compare = "";
+
+    for (let i = valueSize - 1; i > valueSize / 2; i--)    {
+        compare += value[i];
+    }
+
+
+    for (let i = 0; i < compare.length; i++)    {
+        if (compare[i] != value[i])    {return false;}
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
