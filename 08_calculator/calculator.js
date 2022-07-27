6e382cfc9c31a1ab4f569886ed41ae44b1fa68d2
Monito <phil.menard1668@gmail.com>
2022-07-26 22:22:30 -0300

    Finish exercice 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
 const sum = function() {
-	
+
+  let numbers = arguments[0];
+  let result = 0;
+
+  for (let i = 0; i < numbers.length; i++)  {
+    result += +numbers[i];
+  }
+
+  return result;
 };
 
 const multiply = function() {
 
+  let numbers = arguments[0];
+  let result = 1;
+
+  for (let i = 0; i < numbers.length; i++)  {
+    result *= +numbers[i];
+  }
+
+  return result;
+
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+
+  let result = 1;
+	if (number > 1) {
+    for (let i = 1; i <= number; i++)  {
+      result *= i;
+    }
+    return result;
+  }
+  else  {
+    return 1;
+  }
 };
 
 // Do not edit below this line
