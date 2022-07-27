afa076a5f617567b233227940619bbfb50ca8aae
Monito <phil.menard1668@gmail.com>
2022-07-27 12:26:19 -0300

    Finish exercice 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+    let result = 1;
+    let lastNumber = 0;
+
+    //fibonacci(4) returns the 4th member of the series: 3  (1, 1, 2, 3)
+    if (number < 0)    {return "OOPS";}
+
+    for (let i = 1; i < number; i++)   {
+
+        let temp = result;
+        result += lastNumber;
+        lastNumber = temp;
+        //console.log("last number => " + lastNumber);
+    }
+
+    console.log("result => " + result);
+
+    return result;
 
 };
 
