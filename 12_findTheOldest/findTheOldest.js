b0d75f8f87e5e62708ffc684612bfdf295b485a0
Monito <phil.menard1668@gmail.com>
2022-07-27 14:35:26 -0300

    Finish exercice 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let birthDate = people.map(a => a.yearOfBirth);
+    let today = new Date();
+
+    let deathDate = people.map(a => a.yearOfDeath);
+    for (let i = 0; i < deathDate.length; i++)   {
+        if (deathDate[i] == undefined)  {
+            deathDate[i] = today.getFullYear();
+        }
+    }
+
+    let calculAge = [0];
+    let index = 0;
+    let temp = 0;
+
+    for (let i = 0; i < people.length; i++)    {
+        calculAge[i] = deathDate[i] - birthDate[i];
+    }
+
+    for (let i = 0; i < calculAge.length; i++)  {
+    
+        if (calculAge[i] > temp)    {
+            temp = calculAge[i];
+            index = i;
+        }
+    }
+
+    return people[index];
 
 };
 
