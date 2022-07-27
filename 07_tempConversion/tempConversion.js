2eddffc3c743e8b4e5a956f024e5f3dda6486f85
Monito <phil.menard1668@gmail.com>
2022-07-26 21:26:24 -0300

    Finish exercice 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
+const ftoc = function(degrees) {
+
+  // [°C] = ([°F] − 32) × 5⁄9
+  let result = (degrees - 32) * (5/9);
+  return Math.round(result * 10) / 10;
 
 };
 
-const ctof = function() {
+const ctof = function(degrees) {
 
-};
+  //(0 °C × 9/5) + 32
+  let result = (degrees * (9/5)) + 32;
+  return Math.round(result * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
