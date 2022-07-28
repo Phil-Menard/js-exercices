62eae1bf71c1fd5e26e4a9ea809a19f4c4eb2242
Monito <phil.menard1668@gmail.com>
2022-07-28 15:42:33 -0300

    Finish the snakeCase exercice.

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,52 @@
-const snakeCase = function() {
+const snakeCase = function(string) {
 
+    // A-Za-z
+    let result = "";
+    let onlyLetters = true;
+
+    for (let i = 0; i < string.length; i++)    {
+        let currentLetter = string[i];
+
+        if (!currentLetter.match(["^[a-zA-Z]+$"]))  {
+            onlyLetters = false;
+            result = "";
+            break;
+        }
+        else    {
+            if (i != 0 && currentLetter == currentLetter.toUpperCase()) {
+                result += "_" + currentLetter;
+            }
+            else    {
+                result += currentLetter;
+            }
+        }
+    }
+    //SnAkE..CaSe..Is..AwEsOmE
+    if (!onlyLetters)    {
+
+        for (let i = 0; i < string.length; i++) {
+    
+            let currentLetter = string[i];
+    
+            if (currentLetter == " " || currentLetter == "-")   { // if letter is space
+                result += "_";
+            }
+            else if (currentLetter.match(["^[a-zA-Z]+$"]) ) {
+                if (currentLetter == currentLetter.toUpperCase() && string[i-1] == "." && string[i-2] == ".")   {
+                    result += "_" + currentLetter;
+                }
+                else     {
+
+                    result += currentLetter;
+                }
+            }
+    
+        }
+    }
+
+
+    let answer = result.toLowerCase();
+    return answer;
 };
 
 // Do not edit below this line
